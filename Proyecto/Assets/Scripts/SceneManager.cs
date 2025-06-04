0284ddc862f2d96ba37678587e5790aece71a6dd
Gotero <manuotrcarrillo@gmail.com>
2025-06-04 15:02:41 +0200

    IntroCutscene

--- Proyecto/Assets/Scripts/SceneManager.cs
+++ Proyecto/Assets/Scripts/SceneManager.cs
@@ -19,4 +19,9 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Game Quit!"); 
         Application.Quit(); 
     }
+
+    public void LoadIntro()
+    {
+        SceneManager.LoadScene("IntroCutscene");
+    }
 }
